0328a085db8fb11208677153bd379258788c2a12
arcoders <ismaelhaytamtanane@gmail.com>
2023-02-25 12:11:50 +0100

    improve card component

--- src/components/Card/index.js
+++ src/components/Card/index.js
@@ -1,17 +1,16 @@
 import React from 'react'
-import placeholderImg from '../../placeholder.png'
 import { Card } from '../../style'
+import placeholderImg from '../../placeholder.png'
+import { POSTER_NO_DISPONIBLE } from '../../constant'
 
-function Film({ Poster, Year, Title, Type }) {
+function Film({ Poster: poster, Year: year, Title: title, Type: type }) {
+  const posterImage = poster === POSTER_NO_DISPONIBLE ? placeholderImg : poster
   return (
     <Card.Container>
-      <Card.Cover
-        src={Poster === 'N/A' ? placeholderImg : Poster}
-        alt={`${Title} cover`}
-      />
+      <Card.Cover src={posterImage} alt={title} />
       <Card.data>
-        <Card.Title>{Title}</Card.Title>
-        <Card.Meta>{`${Type} | ${Year}`}</Card.Meta>
+        <Card.Title>{title}</Card.Title>
+        <Card.Meta>{type | year}</Card.Meta>
       </Card.data>
     </Card.Container>
   )
