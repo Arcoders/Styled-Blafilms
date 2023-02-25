cb5ff8e81f6cdc6264adb8b38b2482eb89fcda47
arcoders <ismaelhaytamtanane@gmail.com>
2023-02-25 12:21:13 +0100

    improve find method

--- src/services/index.js
+++ src/services/index.js
@@ -2,11 +2,14 @@ import { KEY, BASE_URL, DEFAULT_ERROR_MESSAGE } from './config'
 
 const Movie = {
   find: async ({ query, currentPage }) => {
-    const search = query ? `&s=${query}` : ''
-    const page = currentPage ? `&page=${currentPage}` : ''
+    const params = new URLSearchParams()
+
+    params.set('apikey', KEY)
+    params.set('page', currentPage)
+    query && params.set('s', query)
 
     try {
-      const response = await fetch(`${BASE_URL}/?apikey=${KEY}${search}${page}`)
+      const response = await fetch(`${BASE_URL}?${params}`)
       return await response.json()
     } catch (error) {
       throw Error(DEFAULT_ERROR_MESSAGE)
